9a0ca873069d67b959e43762c5c68f41f511b7d4
Aravind RaJ <aravinu19@gmail.com>
2018-12-20 06:58:55 +0530

    Added SQL Handler

---
+++ SQL_Handler.js
@@ -0,0 +1,22 @@
+var sqlite3 = require('sqlite3').verbose();
+
+var create_db = function(db_name, callback){
+
+};
+
+var insert_to_db = function(data_to_insert, db_name, callback){
+
+};
+
+var remove_word_from_db = function(word_to_remove, db_name, callback){
+
+};
+
+var list_data_from_db = function(db_name){
+
+};
+
+module.exports.create_db = create_db;
+module.exports.insert_to_db = insert_to_db;
+module.exports.remove_word_from_db = remove_word_from_db;
+module.exports.list_data_from_db = list_data_from_db;
